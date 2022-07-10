04f80eca016627bc212ca13dc72830f0a4f96203
Giovanni Zaarour <gzaarour@usc.edu>
2022-07-09 19:13:33 -0700

    Fixed resume for mobile

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,10 +5,8 @@ function Resume() {
     const [embedURL]=useState("https://giozaarour.github.io/resume/Giovanni%20Zaarour%20Resume.pdf")
   return (
     <div className="resume">
-        <object data={embedURL} type="application/pdf" width="100%" height="600px"> 
-            <p>It appears you don't have a PDF plugin for this browser.
-            No biggie... you can <a href={embedURL}>click here to
-            view the PDF file.</a></p>  
+        <object data={embedURL} type="application/pdf" width="100%" height="800px">
+          <embed src={embedURL} width="100%" height="800px"/> 
         </object>
     </div>
   )
